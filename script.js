ef3712f5cda8f73a4232ff30bc383f57d9617263
nicktheuser123 <49652976+nicktheuser123@users.noreply.github.com>
2026-03-13 06:58:07 +0530

    Initial structure of project + todos

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const gameboard = (()=>{
+
+    return {}
+
+})();
+/*
+Todo 
+gameboard as an array on GB object
+
+players also on objects
+
+game object to control the flow of the game
+
+rules
+1 no global code
+
+Get it to work in console first
+game end logic
+3 in a rows and ties
+
+Rest dom and ui related tasks 
+
+
+
+*/
